5c3bf6c4788aa163efa6d8b78131642235df627f
BILLIEBAXTER <hello.billiebaxter@gmail.com>
2023-08-17 11:19:27 +0100

    update

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,33 @@
+import { getPosts, getPostBySlug } from "@/lib/posts";
+import { notFound } from "next/navigation";
+
+type BlogPostParams = {
+  params: {
+    slug: string;
+  };
+};
+
+export function generateStaticParams() {
+  const posts = getPosts();
+
+  return posts.map((post) => {
+    return {
+      slug: post.slug,
+    };
+  });
+}
+
+export default function BlogPost({ params }: BlogPostParams) {
+  const post = getPostBySlug(params.slug);
+
+  if (!post) {
+    notFound();
+  }
+
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
+    </div>
+  );
+}
